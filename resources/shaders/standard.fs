dde2e148c118cc00ab4edce7d26792020fd22013
Jon Bardin <diclophis@gmail.com>
2020-11-18 21:14:46 -0500

    wip shader bits

--- resources/shaders/standard.fs
+++ resources/shaders/standard.fs
@@ -1,8 +1,7 @@
 precision highp float;
 
-// Input vertex attributes (from vertex shader)
 #ifdef NEWER_GL
-in vec3 fragPosition;
+in vec3 fragPositionn;
 in vec2 fragTexCoord;
 in vec4 fragColor;
 in vec3 fragNormal;
@@ -13,16 +12,14 @@ varying vec4 fragColor;
 varying vec3 fragNormal;
 #endif
 
-// Input uniform values
+
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
 
-// Output fragment color
+
 #ifdef NEWER_GL
 out vec4 finalColor;
 #endif
-
-// NOTE: Add here your custom variables
 
 #define     MAX_LIGHTS              4
 #define     LIGHT_DIRECTIONAL       0
@@ -46,19 +43,20 @@ struct Light {
     float coneAngle;
 };
 
-// Input lighting values
+
 uniform Light lights[MAX_LIGHTS];
 uniform vec4 ambient;
 uniform vec3 viewPos;
-//uniform mat4 matModel;
+uniform mat4 matModel;
+
 
 const float glossiness = 0.0001;
-//const vec4 colSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
-vec3 ComputeLightPoint(Light l, vec3 n, vec3 v, vec3 s)
+
+vec3 ComputeLightPoint(Light l, vec3 n, vec3 s)
 {
     //calculate the vector from this pixels surface to the light source
-    vec3 surfaceToLight = l.position - fragPosition;
+    vec3 surfaceToLight = l.position;
 
     //calculate the cosine of the angle of incidence
     float brightness = dot(n, surfaceToLight) / (length(surfaceToLight) * length(n));
@@ -75,130 +73,118 @@ vec3 ComputeLightPoint(Light l, vec3 n, vec3 v, vec3 s)
 }
 
 
-vec3 ComputeLightDirectional(Light l, vec3 n, vec3 v, vec3 s)
+vec3 ComputeLightDirectional(Light l, vec3 n, vec3 s)
 {
     vec3 lightDir = -normalize(l.target - l.position);
 
-    // Diffuse shading
-    //TODO: 
     float diff = clamp(float(dot(n, lightDir)), 0.0, 1.0)*l.intensity;
-    //float diff = (float(dot(n, lightDir)))*l.intensity;
-
-    //// Specular shading
-    //float spec = 0.0;
-    //if (diff > 0.0)
-    //{
-    //    vec3 h = normalize(lightDir + v);
-    //    spec = pow(abs(dot(n, h)), 3.0 + glossiness)*0.3;
-    //}
-    //
-    //// Combine results
-    //return (diff*l.intensity*l.color.rgb + spec*s.rgb);
 
     return (diff * l.color.rgb);
 }
 
 
-vec3 ComputeLightSpot(Light l, vec3 n, vec3 v, vec3 s)
+//vec3 ComputeLightSpot(Light l, vec3 n, vec3 s)
+//{
+//    vec3 lightToSurface = normalize(fragPosition - l.position);
+//    vec3 lightDir = -normalize(l.target - l.position);
+//    float diff = clamp(float(dot(n, lightDir)), 0.0, 1.0)*l.intensity;
+//    float attenuation = (float(dot(n, lightToSurface)));
+//    attenuation = dot(lightToSurface, -lightDir);
+//    float lightToSurfaceAngle = degrees(acos(attenuation));
+//    if (lightToSurfaceAngle > l.coneAngle) attenuation = 0.0;
+//    float falloff = (l.coneAngle - lightToSurfaceAngle)/l.coneAngle;
+//    float diffAttenuation = diff*attenuation;
+//    float spec = 0.0;
+//
+//    return (falloff*(diffAttenuation*l.color.rgb + spec*s.rgb));
+//}
+
+
+vec3 CalcPointLight(Light light, vec3 normal, vec3 fragPos, vec3 viewDir)
 {
-    vec3 lightToSurface = normalize(fragPosition - l.position);
-
-    vec3 lightDir = -normalize(l.target - l.position);
-
-    // Diffuse shading
-    float diff = clamp(float(dot(n, lightDir)), 0.0, 1.0)*l.intensity;
-    //float diff = (float(dot(n, lightDir)))*l.intensity;
-
-    // Spot attenuation
-    //float attenuation = clamp(float(dot(n, lightToSurface)), 0.0, 1.0);
-    float attenuation = (float(dot(n, lightToSurface)));
-    attenuation = dot(lightToSurface, -lightDir);
-
-    float lightToSurfaceAngle = degrees(acos(attenuation));
-    if (lightToSurfaceAngle > l.coneAngle) attenuation = 0.0;
-
-    float falloff = (l.coneAngle - lightToSurfaceAngle)/l.coneAngle;
-
-    // Combine diffuse and attenuation
-    float diffAttenuation = diff*attenuation;
-
-    //// Specular shading
-    float spec = 0.0;
-    if (diffAttenuation > 0.0)
-    {
-        vec3 h = normalize(lightDir + v);
-        spec = pow(abs(dot(n, h)), 3.0 + glossiness)*1.0;
-    }
-
-    return (falloff*(diffAttenuation*l.color.rgb + spec*s.rgb));
-}
+    vec3 lightDir = normalize(light.position - fragPos);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 1.0); //shiny
+    // attenuation
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (0.0 + 1.0 * distance + 
+  			     1.0 * (distance * distance));    
+    // combine results
+    //vec3 ambient  = 0.01 * vec3(1.0); //vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse  = 10.0 * diff * vec3(1.0); //vec3(texture(material.diffuse, TexCoords));
+    vec3 specular = 10.0 * spec * vec3(1.0); //vec3(texture(material.specular, TexCoords));
+    //ambient  *= attenuation;
+    diffuse  *= attenuation;
+    specular *= attenuation;
+    return (diffuse + specular);
+} 
 
 
 void main()
 {
+
+
 #ifndef NEWER_GL
   vec4 finalColor = vec4(0.0);
 #endif
 
 
-// Texel color fetching from texture sampler
 #ifdef NEWER_GL
     vec4 texelColor = texture(texture0, fragTexCoord);
 #else
     vec4 texelColor = texture2D(texture0, fragTexCoord);
 #endif
 
+
     vec3 lightDot = vec3(0.0);
     vec3 normal = normalize(fragNormal);
-    vec3 viewD = normalize(viewPos - fragPosition);
     vec3 specular = vec3(0.0);
 
-    // NOTE: Implement here your fragment shader code
+    //vec4 fpp = matModel * vec4(fragPositionn, 1.0); 
+    //vec3 fpp = fragPositionn;
+    //position_cs = (mModelView * vec4(position,1.0)).xyz;
+    //vec3 viewDir = normalize(viewPos);
+
+    vec3 viewDir = normalize(viewPos - fragPositionn);
+
     for (int i = 0; i < MAX_LIGHTS; i++)
     {
         if (lights[i].enabled == 1)
         {
             vec3 light = vec3(0.0);
 
-  //        if (lights[i].enabled == 1)
-  //        {
-  //            // Calculate lighting based on light type
-  //            if(lights[i].type == LIGHT_POINT) lighting += ComputeLightPoint(lights[i], n, v, specular);
-  //            else if(lights[i].type == LIGHT_DIRECTIONAL) lighting += ComputeLightDirectional(lights[i], n, v, specular);
-  //            else if(lights[i].type == LIGHT_SPOT) lighting += ComputeLightSpot(lights[i], n, v, specular);
-  //            //float NdotL = max(dot(n, lighting), 0.0);
-  //            ////lightDot += lights[i].color.rgb*NdotL;
-  //            //lightDot += NdotL;
-  //        }
-            
             if (lights[i].type == LIGHT_DIRECTIONAL) 
             {
               //light = -normalize(lights[i].target - lights[i].position);
-              light = ComputeLightDirectional(lights[i], normal, viewD, specular);
+              light = ComputeLightDirectional(lights[i], normal, specular);
             }
        
             if (lights[i].type == LIGHT_POINT) 
             {
               //light = normalize(lights[i].position - fragPosition);
-              light = ComputeLightPoint(lights[i], normal, viewD, specular);
+              //light = ComputeLightPoint(lights[i], normal, specular);
+              light = CalcPointLight(lights[i], normal, fragPositionn, viewDir);  
             }
 
             float NdotL = max(dot(normal, light), 0.0);
             lightDot += lights[i].color.rgb*NdotL;
+            //TODO
             float specCo = 0.0;
-            if (NdotL > 0.0) specCo = pow(max(0.0, dot(viewD, reflect(-(light), normal))), 8.0); // 16 refers to shine
+            //if (NdotL > 0.0) specCo = pow(max(0.0, dot(viewD, reflect(-(light), normal))), 8.0); // 16 refers to shine
             specular += specCo;
         }
     }
 
+
     finalColor = (texelColor*((colDiffuse + vec4(specular, 1.0))*vec4(lightDot, 1.0)));
     finalColor += texelColor*(ambient/10.0);
     
-    // Gamma correction
     finalColor = pow(finalColor, vec4(1.0/4.0));
 
-#ifdef NEWER_GL
-#else
+#ifndef NEWER_GL
   gl_FragColor = finalColor;
 #endif
 
